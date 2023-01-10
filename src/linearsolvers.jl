5aa2f83e790bf675aa83d506a489912e5b70952e
yonatanwesen <yonatanwesenyeleh1@gmail.com>
2023-01-10 12:04:23 -0500

    exporting julia functions

---
+++ src/linearsolvers.jl
@@ -0,0 +1,13 @@
+module LibMagma
+# Write your package code here.
+include("../lib/LibMagma.jl")
+
+#exports
+const PREFIXES =["magma_s","magma_d","magma_c","magma_z"]
+for name in names(@__MODULE__;all=true), prefix in PREFIXES
+    if startswith(name,prefix)
+        @eval export $name
+    end
+end
+
+end
