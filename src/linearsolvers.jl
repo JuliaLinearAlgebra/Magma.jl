5c86e59bd330cedf6cfb97a62b5c7007d1beb1d0
yonatanwesen <yonatanwesenyeleh1@gmail.com>
2023-01-11 15:55:39 -0500

    Making name a string

--- src/linearsolvers.jl
+++ src/linearsolvers.jl
@@ -5,7 +5,7 @@ include("../lib/LibMagma.jl")
 #exports
 const PREFIXES =["magma_s","magma_d","magma_c","magma_z"]
 for name in names(@__MODULE__;all=true), prefix in PREFIXES
-    if startswith(name,prefix)
+    if startswith(string(name),prefix)
         @eval export $name
     end
 end
