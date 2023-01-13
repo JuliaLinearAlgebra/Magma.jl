4af8ab6572abc3cf1e2f8635818f7a75c7729a00
yonatanwesen <yonatanwesenyeleh1@gmail.com>
2023-01-13 16:39:35 -0500

    Added some test for gels

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
-using .Magma
-using .Magma: gesv!,gels!
+using Main.Magma
+using Main.Magma: gesv!,gels!
 using Test
-using LinearAlgebra
+using Main.LibMagma
+using LinearAlgebra,Random
 
 @testset "Magma.jl" begin
     # Write your tests here.
@@ -11,6 +12,18 @@ using LinearAlgebra
             @test_throws DimensionMismatch gels!('N',A8x8,B9x9)
             @test_throws DimensionMismatch gels!('T',A8x8,B9x9)
             @test_throws DimensionMismatch gesv!(A8x8,B9x9)
+        end
+    end
+    @testset "gels" begin
+        @testset for elty in (Float32, Float64, ComplexF32, ComplexF64)
+            Random.seed!(913)
+            A = rand(elty,10,10)
+            X = rand(elty,10)
+            magma_init()
+            Y= gels!('N',copy(A),copy(X))
+            magma_finalize()
+            @test A\X ≈ Y
+            
         end
     end
 end
