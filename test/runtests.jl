1a8ccb65dd762b3db339bda81bac266b7cb2cb64
yonatanwesen <yonatanwesenyeleh1@gmail.com>
2023-01-27 13:28:33 -0500

    Added more tests for gesv and created tests for gels

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,8 +9,16 @@ import LinearAlgebra.LAPACK: gels! as lgels!,gesv! as lgesv!
 
 @testset "Magma.jl" begin
     # Write your tests here.
+    @testset "dimension mistach" begin
+        for elty in (Float32, Float64, ComplexF32, ComplexF64)
+            A8x8, B9x9 = Matrix{elty}.(undef, ((8,8), (9,9)))
+            @test_throws DimensionMismatch gels!('N',A8x8,B9x9)
+            @test_throws DimensionMismatch gels!('T',A8x8,B9x9)
+            @test_throws DimensionMismatch gesv!(A8x8,B9x9)
+        end
+    end
     @testset "gesv" begin
-        @testset for elty in (Float32,)
+        @testset for elty in (Float32,Float64,ComplexF32,ComplexF64)
             #Random.seed!(913)
             A = rand(elty,10,10)
             X = rand(elty,10,10)
@@ -18,13 +26,37 @@ import LinearAlgebra.LAPACK: gels! as lgels!,gesv! as lgesv!
             X_cop=copy(X)
             expected_res=lgesv!(A,X)
             magma_init()
-            println("initiated magma")
+            #println("initiated magma")
             # we seg fault in gesv! call
             actual_res= gesv!(A_cop,X_cop)
-            println("after calling the function gesv")
+            #println("after caling the function gesv")
             magma_finalize()
-            @test Array(actual_res[1]) ≈ Array(expected_res[1])
+            for i in 1:3
+                #println("inside the for loop")
+                @test Array(actual_res[i]) ≈ Array(expected_res[i])
+            end
             
+            
+        end
+    end
+
+    @testset "gels" begin
+        @testset for elty in (Float32,Float64,ComplexF32,ComplexF64)
+            A = rand(elty,10,10)
+            X = rand(elty,10,10)
+            A_cop=copy(A)
+            X_cop=copy(X)
+            expected_res=lgels!('N',A,X)
+            magma_init()
+            #println("initiated magma")
+            # we seg fault in gesv! call
+            actual_res= gels!('N',A_cop,X_cop)
+            #println("after caling the function gesv")
+            magma_finalize()
+            for i in 1:3
+                #println("inside the for loop")
+                @test Array(actual_res[i]) ≈ Array(expected_res[i])
+            end 
         end
     end
 end
