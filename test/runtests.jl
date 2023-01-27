1812f4f1cb64c6aeed475d8b891ca9d4e943c0d9
yonatanwesen <yonatanwesenyeleh1@gmail.com>
2023-01-27 14:15:30 -0500

    Added tests for posv

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
 using Magma
-using Magma: gesv!,gels!,magma_init,magma_finalize
+using Magma: gesv!,gels!,posv!,magma_init,magma_finalize
 using Test
 using Random
 #using libstramopil
 #using Main.LibMagma
-import LinearAlgebra.LAPACK: gels! as lgels!,gesv! as lgesv!
+using LinearAlgebra
+import LinearAlgebra.LAPACK: gels! as lgels!,gesv! as lgesv!, posv! as lposv!
 #using libblastrampoline_jll
 
 @testset "Magma.jl" begin
@@ -59,4 +60,32 @@ import LinearAlgebra.LAPACK: gels! as lgels!,gesv! as lgesv!
             end 
         end
     end
+
+    @testset "posv" begin
+        @testset for elty in (Float32,Float64,ComplexF32,ComplexF64)
+            A = rand(elty,10,10)/100
+            A += real(diagm(0 => 10*real(rand(elty,10))))
+            if elty <: Complex
+                A = A + A'
+            else
+                A = A + transpose(A)
+            end
+            X = rand(elty,10,10)
+            A_cop = copy(A)
+            X_cop = copy(X)
+            expected_res=lposv!('U',A,X)
+            magma_init()
+            #println("initiated magma")
+            # we seg fault in gesv! call
+            actual_res= posv!('U',A_cop,X_cop)
+            #println("after caling the function gesv")
+            magma_finalize()
+            for i in 1:2
+                #println("inside the for loop")
+                @test Array(actual_res[i]) ≈ Array(expected_res[i])
+            end 
+        end
+    end
+
+
 end
