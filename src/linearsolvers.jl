0ee3dda5583264894eeedd79bdea5fb401964978
Will Kimmerer <kimmerer@mit.edu>
2023-03-16 16:53:56 -0400

    add CUDA to test deps

--- src/linearsolvers.jl
+++ src/linearsolvers.jl
@@ -2,7 +2,7 @@ module LibMagma
 # Write your package code here.
 using MAGMA_jll
 using CUDA
-const libmagma=MAGMA_jll.libmagma_path
+const libmagma = MAGMA_jll.libmagma_path
 
 export magma_init
 export magma_finalize
